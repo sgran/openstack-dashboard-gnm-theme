3ca58cf85278ecead886dda00dcac118941dbae6
Stephen Gran <steve@lobefin.net>
2013-08-24 14:23:11 +0100

    Initial release Signed-off-by: Stephen Gran <steve@lobefin.net>

---
+++ theme/gnm_theme.py
@@ -0,0 +1,6 @@
+# The presence of this file in /etc/openstack-dashboard/ and/or
+# /usr/share/openstack-dashboard/openstack_dashboard/local/ will
+# enable the GNM theme for Horizon.  To disable, remove the
+# openstack-dashboard-gnm-theme package, or remove this file.
+TEMPLATE_DIRS = ('/usr/share/openstack-dashboard-gnm-theme/templates', )
+
